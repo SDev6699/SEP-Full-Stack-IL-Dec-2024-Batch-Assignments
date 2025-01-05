07e0d254785e4d1b214c78386a818e9df7404f7f
Sahil Atul Darandale <sdarandale0699@gmail.com>
2025-01-05 14:59:52 -0500

    Add RandomNumberGuess console application

---
+++ 03_Loops_and_Operators/RandomNumberGuess/RandomNumberGuess/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RandomNumberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int correctNumber = random.Next(1, 4); // Generates 1, 2, or 3
+
+            Console.Write("Guess the number (1-3): ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int guessedNumber))
+            {
+                if (guessedNumber < 1 || guessedNumber > 3)
+                {
+                    Console.WriteLine("Your guess is out of the valid range (1-3).");
+                }
+                else if (guessedNumber < correctNumber)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                }
+                else if (guessedNumber > correctNumber)
+                {
+                    Console.WriteLine("Your guess is too high.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the correct number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer between 1 and 3.");
+            }
+        }
+    }
+}
