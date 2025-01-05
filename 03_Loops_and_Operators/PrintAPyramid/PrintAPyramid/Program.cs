d86a3d556d41552aab0a733001035c0ee25bfec3
Sahil Atul Darandale <sdarandale0699@gmail.com>
2025-01-05 15:31:49 -0500

    Add PrintAPyramid console application

---
+++ 03_Loops_and_Operators/PrintAPyramid/PrintAPyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrintAPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = 5;
+
+            for (int i = 1; i <= numberOfRows; i++)
+            {
+                // Print spaces
+                for (int j = 1; j <= numberOfRows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print stars
+                for (int k = 1; k <= (2 * i - 1); k++)
+                {
+                    Console.Write("*");
+                }
+
+                // Move to next line
+                Console.WriteLine();
+            }
+        }
+    }
+}
