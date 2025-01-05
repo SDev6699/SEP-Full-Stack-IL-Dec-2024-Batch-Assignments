3e539d1f7056fd14ed03a67d9c718ffefeffa369
Sahil Atul Darandale <sdarandale0699@gmail.com>
2025-01-05 14:21:34 -0500

    Add HackerNameGenerator console application

---
+++ 01_Introduction_to_CSharp_and_DataTypes/PlayingWithConsoleApp/HackerNameGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HackerNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hacker Name Generator!");
+
+            Console.Write("Enter your favorite color: ");
+            string color = Console.ReadLine();
+
+            Console.Write("Enter your astrology sign: ");
+            string sign = Console.ReadLine();
+
+            Console.Write("Enter your street address number: ");
+            string addressNumber = Console.ReadLine();
+
+            string hackerName = $"{color}{sign}{addressNumber}";
+            Console.WriteLine($"Your hacker name is {hackerName}.");
+        }
+    }
+}
