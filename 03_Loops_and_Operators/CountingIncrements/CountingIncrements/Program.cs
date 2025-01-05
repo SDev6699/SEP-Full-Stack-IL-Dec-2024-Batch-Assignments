4036c25f3dbd00477ffbf244e8ecb701484922f0
Sahil Atul Darandale <sdarandale0699@gmail.com>
2025-01-05 15:47:17 -0500

    Add CountingIncrements console application

---
+++ 03_Loops_and_Operators/CountingIncrements/CountingIncrements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountingIncrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 24;
+
+            for (int increment = 1; increment <= 4; increment++)
+            {
+                for (int i = 0; i <= max; i += increment)
+                {
+                    if (i != 0)
+                        Console.Write(",");
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
