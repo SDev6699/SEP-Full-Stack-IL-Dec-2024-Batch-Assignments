4a7b00ff405ae2d9fd7dfd63746a6a1cc6ba8fd6
Sahil Atul Darandale <sdarandale0699@gmail.com>
2025-01-05 16:12:27 -0500

    Add ReverseString console application to PracticeStrings

---
+++ 02_Arrays_and_Strings/PracticeStrings/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+
+            // Method 1: Using Char Array
+            string reversedUsingCharArray = ReverseUsingCharArray(input);
+            Console.WriteLine("Reversed using Char Array: " + reversedUsingCharArray);
+
+            // Method 2: Using For-Loop
+            string reversedUsingForLoop = ReverseUsingForLoop(input);
+            Console.WriteLine("Reversed using For-Loop: " + reversedUsingForLoop);
+        }
+
+        static string ReverseUsingCharArray(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static string ReverseUsingForLoop(string str)
+        {
+            string reversed = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            return reversed;
+        }
+    }
+}
