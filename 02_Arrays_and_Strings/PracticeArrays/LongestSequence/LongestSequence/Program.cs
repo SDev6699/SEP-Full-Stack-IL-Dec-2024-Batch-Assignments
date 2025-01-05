04cb59b441c12c4c107591c8b96d4fc5cdc0e7a7
Sahil Atul Darandale <sdarandale0699@gmail.com>
2025-01-05 16:05:36 -0500

    Add LongestSequence console application to PracticeArrays

---
+++ 02_Arrays_and_Strings/PracticeArrays/LongestSequence/LongestSequence/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the array elements (space separated): ");
+            string input = Console.ReadLine();
+            int[] array = Array.ConvertAll(input.Split(' '), int.Parse);
+
+            List<int> longestSeq = FindLongestSequence(array);
+
+            if(longestSeq.Count > 0)
+            {
+                Console.WriteLine("Longest sequence: " + string.Join(" ", longestSeq));
+            }
+            else
+            {
+                Console.WriteLine("No elements found.");
+            }
+        }
+
+        static List<int> FindLongestSequence(int[] array)
+        {
+            if(array.Length == 0)
+                return new List<int>();
+
+            List<int> currentSeq = new List<int> { array[0] };
+            List<int> longestSeq = new List<int> { array[0] };
+
+            for(int i = 1; i < array.Length; i++)
+            {
+                if(array[i] == array[i - 1])
+                {
+                    currentSeq.Add(array[i]);
+                }
+                else
+                {
+                    if(currentSeq.Count > longestSeq.Count)
+                    {
+                        longestSeq = new List<int>(currentSeq);
+                    }
+                    currentSeq.Clear();
+                    currentSeq.Add(array[i]);
+                }
+            }
+
+            // Check at the end
+            if(currentSeq.Count > longestSeq.Count)
+            {
+                longestSeq = new List<int>(currentSeq);
+            }
+
+            return longestSeq;
+        }
+    }
+}
